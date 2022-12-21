aa2cb98b4c1b46fc93f27f79924637d6b16e7b90
Andy Vajda <andy.vajda@gmail.com>
2022-12-21 01:34:15 -0600

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,36 @@
 module.exports = {
-  plugins: ["gatsby-plugin-offline", "gatsby-plugin-manifest"],
+  plugins: [
+    {
+      resolve: "gatsby-source-cosmicjs",
+      options: {
+        bucketSlug: "monstertest",
+        objectTypes: ["artists"],
+        apiAccess: {
+          read_key: "dN5RQ2p0zvFMHOWBXs6xWEfMoipsnr8HBAfIXDzzqjP9PtoLaA",
+        },
+        localMedia: true // Download media locally for gatsby image (optional)
+      }
+    },
+    {
+      resolve: "gatsby-plugin-manifest", 
+      options: {
+        name: "Monsters Ink",
+        short_name: "Monsters Ink",
+        start_url: "/",
+        background_color: `#f7f0eb`,
+        theme_color_in_head: false,
+        display: `standalone`,
+        icon: 'icon.jpg',
+        cache_busting_mode: 'none',
+      }
+    },
+    {
+      resolve: "gatsby-plugin-offline",
+      options: {
+        workboxConfig: {
+          globPatterns: ['**/icon-path*'],
+        },
+      }
+    },
+  ]
 };
